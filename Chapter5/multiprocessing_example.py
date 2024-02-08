ea6d904694599076e621c548acda11d6af0c48bb
nithikpackt <77096057+nithikpackt@users.noreply.github.com>
2024-02-08 13:24:28 +0530

    Update multiprocessing_example.py

--- Chapter5/multiprocessing_example.py
+++ Chapter5/multiprocessing_example.py
@@ -10,7 +10,7 @@ def get_total_sum(full_array):
 	pool = multiprocessing.Pool(processes=4)
 	''' List of sums in 10,000 number blocks '''
 	sum_list = []
-	''' Get 10,000 values at a time for sum out of 1,000,000''''
+	''' Get 10,000 values at a time for sum out of 1,000,000 '''
 	for i in range(0, count(full_array)/10000):
 		sum_array = full_array[i*10000:(i+1)*10000]
 		'''Make an array of 10,000 length arrays '''
